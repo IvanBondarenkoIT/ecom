1c6867fe99ec63dc0711c88ad8da7710fe7677b3
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2023-12-08 00:01:32 +0400

    models add: Categories Product Customer

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 import datetime
 
-# Category
+# Categories of Products
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 # Customer
-# Product
-# Order
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Order(models.Model):
+    pass
