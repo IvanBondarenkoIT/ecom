4973a474785f44874a4b43aff8f20eb57ccbc4df
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2023-12-17 11:31:01 +0400

    product pages

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -7,6 +7,12 @@ from django.contrib import messages
 from .forms import SignUpForm
 # from django import forms
 
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
+
+
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
