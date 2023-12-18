68c2bb937154d2c8092b64e9137d2b457abc3035
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2023-12-18 12:41:00 +0400

    product category

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # from django.contrib.auth.models import User
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 # from django import forms
+
+
+def category(request, foo):
+    # Replace Hyphens with Spaces
+    foo = foo.replace('-', ' ')
+    # category = Product.objects.get(id=foo)
+    print(foo)
+    # Grab tht category from the url
+    try:
+        category = Category.objects.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products, 'category': category})
+    except:
+        messages.success(request, ("That category dosen't exist"))
+        return redirect('home')
+    return render(request, 'category.html', {'category': category})
 
 
 def product(request, pk):
