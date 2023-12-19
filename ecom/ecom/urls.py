6a2fe2d1a83690f783553efb256b456803572e18
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2023-12-19 10:59:57 +0400

    shopping cart setup

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("store.urls")),
+    path("cart/", include("cart.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
