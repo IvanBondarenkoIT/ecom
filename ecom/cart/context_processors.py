9d8dca31b4a92e71715aa7860af0f2b0ae08e4ef
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2023-12-20 11:47:05 +0400

    customer sessions

---
+++ ecom/cart/context_processors.py
@@ -0,0 +1,7 @@
+from .cart import Cart
+
+
+# Create context processor so our cart can work on all pages
+def cart(request):
+    # Return the default data from our Cart
+    return {"cart": Cart(request)}
