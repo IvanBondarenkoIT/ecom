f88618ba265d76e0c1c85d742f9e397071891538
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2023-12-23 22:28:22 +0400

    add: cart step 2

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -7,7 +10,22 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    # Get the catr
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get("action") == "post":
+        # Get stuff
+        product_id = int(request.POST.get("product_id"))
+
+        # lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+
+        # Save to session
+        cart.add(product=product)
+
+        # Return response
+        response = JsonResponse({"Product Name: ": product.name})
+        return response
 
 
 def cart_delete(request):
