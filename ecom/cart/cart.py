af3ac7fc924f5f6132a59bb34f2cf51c2478bbe7
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-01-04 22:00:15 +0400

    added: product quantities

--- ecom/cart/cart.py
+++ ecom/cart/cart.py
@@ -15,14 +15,16 @@ class Cart:
         # Make sure cart is available on all pages of site
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         # Logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {"price": str(product.price)}
+            # self.cart[product_id] = {"price": str(product.price)}
+            self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
 
@@ -36,3 +38,7 @@ class Cart:
         products = Product.objects.filter(id__in=product_ids)
         # Return those looked up products
         return products
+
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
