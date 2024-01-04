c01b1fdfb87547db8908cd87dbc4d98d89bc0406
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-01-04 13:50:21 +0400

    added: update cart button

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -23,8 +23,12 @@ def cart_add(request):
         # Save to session
         cart.add(product=product)
 
+        # Get Cart Quantity
+        cart_quantity = cart.__len__()
+
         # Return response
-        response = JsonResponse({"Product Name: ": product.name})
+        # response = JsonResponse({"Product Name: ": product.name})
+        response = JsonResponse({"qty: ": cart_quantity})
         return response
 
 
