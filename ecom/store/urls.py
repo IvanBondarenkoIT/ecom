b7d92c33791ada7c723ac2ae52e3eddffe0586ef
IvanBondarenkoIT <ivan.bondarenko.it@gmail.com>
2024-01-06 14:24:46 +0400

    added: categories directory page

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register'),
-    path('product/<int:pk>', views.product, name='product'),
-    path('category/<str:foo>', views.category, name='category'),
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
+    path("product/<int:pk>", views.product, name="product"),
+    path("category/<str:foo>", views.category, name="category"),
+    path("category_summary", views.category_summary, name="category_summary"),
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,79 +2,91 @@ from django.shortcuts import render, redirect
 from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 # from django.contrib.auth.models import User
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
+
 # from django import forms
+
+
+def category_summary(request):
+    categories = Category.objects.all()
+    return render(request, "category_summary.html", {"categories": categories})
 
 
 def category(request, foo):
     # Replace Hyphens with Spaces
-    foo = foo.replace('-', ' ')
+    foo = foo.replace("-", " ")
     # category = Product.objects.get(id=foo)
     print(foo)
     # Grab tht category from the url
     try:
         category = Category.objects.get(name=foo)
         products = Product.objects.filter(category=category)
-        return render(request, 'category.html', {'products': products, 'category': category})
+        return render(
+            request, "category.html", {"products": products, "category": category}
+        )
     except:
         messages.success(request, ("That category dosen't exist"))
-        return redirect('home')
-    return render(request, 'category.html', {'category': category})
+        return redirect("home")
+    return render(request, "category.html", {"category": category})
 
 
 def product(request, pk):
     product = Product.objects.get(id=pk)
-    return render(request, 'product.html', {'product': product})
+    return render(request, "product.html", {"product": product})
 
 
 def home(request):
     products = Product.objects.all()
-    return render(request, 'home.html', {'products':products})
+    return render(request, "home.html", {"products": products})
 
 
 def about(request):
-    return render(request, 'about.html', {})
+    return render(request, "about.html", {})
 
 
 def login_user(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST["username"]
+        password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, ("You Have Been Logged In!"))
-            return redirect('home')
+            return redirect("home")
         else:
             messages.success(request, ("There was an error, please try again..."))
-            return redirect('login')
+            return redirect("login")
     else:
-        return render(request, 'login.html', {})
+        return render(request, "login.html", {})
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, ("You have been log out..."))
-    return redirect('home')
+    return redirect("home")
 
 
 def register_user(request):
     form = SignUpForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
             # log in user
             user = authenticate(request, username=username, password=password)
             login(request, user)
             messages.success(request, ("You Have Registered Successfully!!!"))
-            return redirect('home')
+            return redirect("home")
         else:
-            messages.success(request, ("Whoops! There was a problem Regestering, please try again..."))
-            return redirect('register')
+            messages.success(
+                request,
+                ("Whoops! There was a problem Regestering, please try again..."),
+            )
+            return redirect("register")
 
-    return render(request, 'register.html', {'form': form})
+    return render(request, "register.html", {"form": form})
